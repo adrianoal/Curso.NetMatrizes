b23b54cf12673cd466b899082375e216a15c26a3
Adriano Lima <adriano.lima85@yahoo.com.br>
2020-06-12 16:57:23 -0300

    Exemplo de Matriz propriedade Length, Rank e GetLength

---
+++ Matrizes/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace MatrizAula79
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[,] mat = new double[2, 3]; // 2 linha e 3 colunas
+
+            Console.WriteLine(mat.Length); //Quantos elementos a matriz tem no total. 
+            Console.WriteLine();
+
+            Console.WriteLine(mat.Rank); // Primeira dimensão da matriz(Qtde de linhas)
+            Console.WriteLine();
+
+            Console.WriteLine(mat.GetLength(0)); //Primeira dimensão(Qtde de linhas da Matriz)
+            Console.WriteLine();
+
+            Console.WriteLine(mat.GetLength(1)); // Segunda dimensão(Qtde de Colunas da Matriz)
+            Console.WriteLine();
+
+        }
+    }
+}
